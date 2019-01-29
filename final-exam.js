65aa3851c5b96d29b3ae7f331ecc8d549c2edc65
Eimy Gonzalez <U91596@ust-global.com>
2019-01-29 11:45:13 -0600

    git commit

--- final-exam.js
+++ final-exam.js
@@ -23,20 +23,55 @@ function fillTable(){
         var col = document.createElement('div');
         var col2 = document.createElement('div');
         var col3 = document.createElement('div');
+        var num = document.createElement('p');
         var button = document.createElement('button');
 
         col.className = "col-sm";
         col2.className = "col-sm";
+        col3.className = "col-sm";
         button.className = "plus-btn";
+        num.id = "num";
+        num.className = "num";
 
         table.append(row);
         row.appendChild(col);
-        col.innerText = product.name;
         row.appendChild(col2);
-        col2.innerText = product.price;
-        row.appendChild(button);
+        row.appendChild(col3)
+        col3.append(num);
+        col3.append(button);
 
+        col.innerText = product.name;
+        col2.innerText = product.price;
+        num.innerText= 0.0;
+        button.innerText = '+';
+
+       
+        button.addEventListener("click", add);
+        //button.onclick = add();
         //document.write(JSON.stringify(products));
  
     });
+}
+
+
+
+function add(){
+    var num = document.getElementById("num");
+    
+    num.innerText += 1;
+}
+
+//var button = document.getElementsByClassName("plus-btn");
+
+function total(){
+    var nums = document.getElementsByClassName("num");
+    document.write(JSON.stringify(nums));
+    /*
+    var total = 0;
+    nums.forEach(num => {
+
+        total = total + num.innerText;
+
+    })*/
+    
 }
